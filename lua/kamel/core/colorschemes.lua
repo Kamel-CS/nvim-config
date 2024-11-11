183d1a1e1f9953ede03b52f08215af3222765be6
Kamel <kameldemri00@gmail.com>
2024-11-11 14:38:56 +0100

    Add colorschemes and gui Nvim files

---
+++ lua/kamel/core/colorschemes.lua
@@ -0,0 +1,3 @@
+require("kamel.lazy")
+-- colorscheme
+vim.cmd("colorscheme rose-pine-main")
---
+++ lua/kamel/core/neovide.lua
@@ -0,0 +1,3 @@
+-- gui nvim (Neovide)
+vim.o.guifont = "JetBrains Mono:h11"  -- Adjust font and size
+vim.g.neovide_scale_factor = 1.0  -- Adjust scaling

257cc93a8fa17fee4c85fec9026b147e0cf30eaa
Kamel Demri <kameldemri00@gmail.com>
2024-12-20 21:45:52 +0100

    change colorscheme

--- lua/kamel/plugins/lualine.lua
+++ lua/kamel/plugins/lualine.lua
@@ -53,7 +53,7 @@ return {
     lualine.setup({
       options = {
         --theme = my_lualine_theme,
-        theme = "rose-pine",
+        theme = "gruvbox",
       },
       sections = {
         lualine_x = {
